c11b2139b685146ec1eeabf66c1a3d00e72964f4
Roman Razumeyko <rawzes@gmail.com>
2021-09-10 14:47:09 +0300

    added step7

---
+++ part2/lesson2.1-step6.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+
+try:
+    browser = webdriver.Chrome()
+    browser.get('http://suninjuly.github.io/math.html')
+
+    people_radio = browser.find_element_by_id("peopleRule")
+    people_checked = people_radio.get_attribute("checked")
+    print("value of people radio: ", people_checked)
+
+    assert people_checked is not None, "People radio is not selected by default"
+    assert people_checked == "true", "People radio is not selected by default"
+
+    robots_radio = browser.find_element_by_id("robotsRule")
+    robots_checked = robots_radio.get_attribute("checked")
+    assert robots_checked is None
+finally:
+    browser.quit()
