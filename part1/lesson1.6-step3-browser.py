3b948858cbfaa6f8c9ba4bf41b777b837b01d4b3
Roman Razumeyko <rawzes@gmail.com>
2021-09-10 10:25:13 +0300

    added step 3

---
+++ part1/lesson1.6-step3-browser.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+browser = webdriver.Chrome()
+browser.get(link)
+button = browser.find_element(By.ID, "submit_button")
+button.click()
+
+# закрываем браузер после всех манипуляций
+browser.quit()
+
+# the same as with...as operator
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element(By.ID, "submit_button")
+    button.click()
+
+finally:
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+    print('test from finally')
