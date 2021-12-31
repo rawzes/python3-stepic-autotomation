754402deb1941705c7e71d9007777ce0a5351934
Roman Razumeyko <rawzes@gmail.com>
2021-12-31 22:25:53 +0300

    Changed depricated method

--- part2/lesson2.3-step6.py
+++ part2/lesson2.3-step6.py
@@ -1,19 +1,22 @@
 from selenium import webdriver
 import time
 import math
+
+
 def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
 
 browser = webdriver.Chrome()
 browser.get('http://suninjuly.github.io/redirect_accept.html')
-#get current window
+# get current window
 current_window = browser.current_window_handle
 print('current window ', current_window)
-#time.sleep(2)
+# time.sleep(2)
 element = browser.find_element_by_tag_name('button')
 element.click()
 print('list windows', browser.window_handles)
-browser.switch_to_window(browser.window_handles[1])
+browser.switch_to.window((browser.window_handles[1]))
 
 x = int(browser.find_element_by_css_selector('#input_value').text)
 browser.find_element_by_css_selector('#input_value')
